dff88181a14481549c91843de120521e3eb7f17f
Mathieu PATUREL <australie.p@gmail.com>
2021-08-24 21:48:09 +1000

    kirikou: fix permission
    use a setup script to setup the container, so that we can run it as
    runner, and not root, so that permissions are good

--- kirikou/install.sh
+++ kirikou/install.sh
@@ -7,18 +7,5 @@ fi
 
 useradd --create-home --system runner
 
-git config --global user.name runner
-git config --global user.email "runner@gitgraph.viz"
-
-cd /home/runner
-mkdir repo
-cd repo
-git init -q
-
-echo "Make some commits!" >> README.md
-echo >> README.md
-echo "You can read the tutorial if you're unsure what to try." >> README.md
-
-git add README.md 
-git commit -q -m "initial commit"
-
+su -c 'bash /setup-user.sh' runner
+rm /setup-user.sh
---
+++ kirikou/setup-user.sh
@@ -0,0 +1,14 @@
+git config --global user.name runner
+git config --global user.email "runner@gitgraph.viz"
+
+cd /home/runner
+mkdir repo
+cd repo
+git init -q
+
+echo "Make some commits!" >> README.md
+echo >> README.md
+echo "You can read the tutorial if you're unsure what to try." >> README.md
+
+git add README.md 
+git commit -q -m "initial commit"
