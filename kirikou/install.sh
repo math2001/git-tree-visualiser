5a3d1000d69e85af446ef15ecf64edfd822c78d7
Mathieu PATUREL <australie.p@gmail.com>
2021-08-24 11:50:08 +1000

    kirikou: automatically initialise git repository

--- kirikou/install.sh
+++ kirikou/install.sh
@@ -14,7 +14,8 @@ for ((i=0; i<$NUMBER_USER;i++))
 do
         useradd --create-home --groups=runners --no-user-group --system "runner-$i"
         chmod 700 "/home/runner-$i"
-        su -c "git config --global user.name runner-$i; git config --global user.email 'runner-$i@gitgraph.viz'" "runner-$i"
+        su -c "bash /setup-user.sh" "runner-$i"
 done
 
 echo "umask u=rwx,g=,o=" >> /etc/profile.d/02-set-umask.sh
+rm /setup-user.sh
